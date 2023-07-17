66522632c54d7b9f3fdfd24ba7d5cd52bdc97266
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-07-17 18:49:47 +0200

    scaffolding + get data from axios

---
+++ src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from 'Axios'
+export default{
+    name:'Appmain',
+    data(){
+        return{
+            ApiUrl:'http://localhost:8000/api/projects',
+        }
+    },
+    mounted(){
+        axios.get(this.ApiUrl).then(resp => {
+            let callToApi = resp.data;
+            console.log(callToApi);
+        })
+    },
+}
+</script>
+<template>
+    <div>
+        ciao 
+    </div>
+</template>
