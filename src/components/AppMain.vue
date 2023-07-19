6dfe381f13d317d0cdb5dba7e1bff6e21c39f462
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-07-19 17:43:56 +0200

    display basic single project

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,37 +2,41 @@
 import axios from 'Axios'
 import ProjectCard from './ProjectCard.vue';
 
-export default{
-    name: "Appmain",
-    components: { 
-        ProjectCard 
-    },
-    data() {
-        return {
-            ApiUrl: "http://localhost:8000/api/projects",
-            callToApi: "",
-            projects: [],
-            loadingMessage:''
-        };
-    },
-    methods: {
-        getApiData() {
-            axios.get(this.ApiUrl).then(resp => {
-                this.callToApi = resp.data;
-                this.projects = this.callToApi.results;
-                console.log(this.projects)
-            }).catch( err => {
-                this.loading=false;
-                console.log(err)
-                this.loadingMessage = err.message;
-                this.$router.push({name:'errore' , params: { code: 404 }})
-            });
-        }
-    },
-    mounted() {
-        this.getApiData();
-    },
-}
+    export default{
+
+        name: "Appmain",
+
+        components: { 
+            ProjectCard,
+        },
+        data() {
+            return {
+                ApiUrl: "http://localhost:8000/api/projects",
+                callToApi: "",
+                projects: [],
+                loadingMessage:''
+            };
+        },
+
+        methods: {
+            getApiData() {
+                axios.get(this.ApiUrl).then(resp => {
+                    this.callToApi = resp.data;
+                    this.projects = this.callToApi.results;
+                    console.log(this.projects)
+                }).catch( err => {
+                    this.loading=false;
+                    console.log(err)
+                    this.loadingMessage = err.message;
+                    this.$router.push({name:'errore' , params: { code: 404 }})
+                });
+            },
+        },
+        
+        mounted() {
+            this.getApiData();
+        },
+    }
 </script>
 <template>
     <h1 class="text-6xl font-semibold text-center mt-16 text-white">All Project preview</h1>
--- src/components/pages/SingleProject.vue
+++ src/components/pages/SingleProject.vue
@@ -1,46 +1,67 @@
 <script>
-import axios from 'axios'
+import axios from 'axios';
 
-    export default{ 
-        name:'SinglePost',
-        data() {
-            return {
-                ApiUrl: "http://localhost:8000/api/projects/",
-                loading:false,
-                loadingMessage:false,
-                project:{}
-            }
-        },
-        methods: {
-            prova(){
-                console.log('ciao');
-            },
-
-            getApiData(id) {
-            axios.get(this.ApiUrl + id ).then(resp => {
-                this.loading=true;
-                this.callToApi = resp.data;
-                this.project = this.callToApi.results;
-                console.log(this.ApiUrl + id )
-                console.log(this.project)
-            }).catch( err => {
-                this.loading=false;
-                console.log(err)
-                this.loadingMessage = err.message;
-                this.$router.push({name:'errore' , params: { code: 404 }})
-            });
-        },
-        mounted() {
-            this.prova(),
-            this.getApiData($route.params.id);
-        },
-        },
+export default {
+  name: 'SinglePost',
+  data() {
+    return {
+      ApiUrl: "http://localhost:8000/api/projects/",
+      imgPath:'http://127.0.0.1:8000/storage/',
+      loading: false,
+      loadingMessage: false,
+      project: {},
     }
+  },
+  methods: {
+    prova() {
+      console.log('ciao');
+    },
+    getApiData(id) {
+      axios
+        .get(this.ApiUrl + id)
+        .then((resp) => {
+          this.loading = true;
+          this.callToApi = resp.data;
+          this.project = this.callToApi.results;
+          this.loading = false;
+          console.log(this.ApiUrl + id)
+          console.log(this.project)
+        })
+        .catch((err) => {
+          this.loading = false;
+          console.log(err)
+          this.loadingMessage = err.message;
+          this.$router.push({ name: 'errore', params: { code: 404 } })
+        });
+    }
+  },
+  mounted() {
+    this.prova(); // Chiamata corretta per eseguire la funzione prova
+    this.getApiData(this.$route.params.id); // Corretta sintassi per accedere a $route.params.id
+  },
+}
+
+
 </script>
 <template>
-
-    <div class="w-96 m-8 p-6 rounded bg-amber-300" @click="this.getApiData($route.params.id);">
+    <div v-if="this.loading === false">
+        <div class="w-96 m-8 p-6 rounded bg-amber-300">
         {{ $route.params.id}}
+        <div class="w-6 h-6 bg-blue-500 rounded-full mx-auto my-2 flex items-center">
+            <div class="w-4 h-4 bg-blue-900 rounded-full mx-auto"></div>
+        </div>
+        <h2 class="text-4xl text-center pt-8">
+            {{ this.project.title}}
+        </h2>
+        <p class="text-center mx-auto w-56">
+            {{ this.project.content}}
+        </p>
+        <img :src="this.imgPath + this.project.image" alt="" class="rounded-2xl">
+        
+        <p class="text-center pt-3 mx-auto w-56 font-bold">
+           <!-- project type: {{ this.project}} -->
+        </p>
+    </div>
     </div>
 </template>
 
